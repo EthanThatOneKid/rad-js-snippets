a25bb39e79f4572794744bec8b08864253d83c02
EthanThatOneKid <31261035+EthanThatOneKid@users.noreply.github.com>
2019-07-01 13:19:31 -0700

    Create math-snippets.js

---
+++ math-snippets.js
@@ -0,0 +1,15 @@
+const gcd = (a, b) => !b ? a : gcd(b, b % a);
+
+const reduce = (a, b) => {
+    const c = gcd(a, b);
+    return c > 1
+        ? reduce(a / c, b / c)
+        : [a, b];
+};
+
+const reduce2nArr = (a) => {
+    const b = gcd(a[0], a[1]);
+    return b > 1
+        ? reduce2nArr(a.map(c => c / b))
+        : a;
+};
