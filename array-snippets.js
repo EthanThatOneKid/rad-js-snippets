5d95a11aef0c8b0aedfc48095005dd03af958e5c
EthanThatOneKid <31261035+EthanThatOneKid@users.noreply.github.com>
2019-08-01 22:25:10 -0700

    Update array-snippets.js

--- array-snippets.js
+++ array-snippets.js
@@ -1,1 +1,3 @@
 const toLinkedList = a => a.reduceRight((acc, cur) => ({val: cur, next: acc}), null);
+
+const reverseLinkedList = (list, memo = null) => list == null ? memo : reverseLinkedList(list.next, {val: list.val, next: memo});
