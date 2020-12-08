0cf2bf2d37097c029a874f4e7970fc4da47b5b16
EthanThatOneKid <31261035+EthanThatOneKid@users.noreply.github.com>
2020-12-08 15:15:52 -0800

    Update function-snippets.js

--- function-snippets.js
+++ function-snippets.js
@@ -1,1 +1,19 @@
 const chainify = (...chain) => arg => chain.reduce((a, b) => b(a), arg);
+
+const sum = (num, ...nums) => nums.length > 0
+  ? num + sum(nums.shift(), ...nums)
+  : num;
+
+const checkArraysAreIdentical = (array1, array2, ...arrays) => {
+  if (array1.length !== array2.length) {
+    return false;
+  }
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  }
+  return arrays.length > 0
+    ? checkArraysAreIdentical(array2, arrays.shift(), ...arrays)
+    : true;
+};
